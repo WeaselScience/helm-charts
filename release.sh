b0140cc69356844ac186d07d8527caa910cdbb20
weasel.science <algiuxass@gmail.com>
2021-11-17 10:35:55 +0000

    dump

---
+++ release.sh
@@ -0,0 +1,12 @@
+git checkout main
+helm package argo-tunnel
+helm repo index .
+git add -A
+git commit -m "dump"
+git push
+git checkout gh-pages
+git checkout main -- index.yaml
+git checkout main -- argo-tunnel-*
+git commit -m "dump"
+git push
+git checkout main
